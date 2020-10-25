efedab9a5f0e3e93189bba43bd56a0d8111f8263
yuria <yuria.soft0615@gmail.com>
2020-10-25 20:53:41 +0900

    話しかける部分のAPIを実装した。

--- src/api/Chat.js
+++ src/api/Chat.js
@@ -1,10 +1,32 @@
 exports.handler = function(event ,context ,callback){
-let msg="Hello,LaLiMaAPI";
+    const msgList = [
+        "ねーねー。",
+        "きいてるー？？",
+        "はなそーよー",
+        "大好きだよ",
+        "愛してるよー！",
+        "すき……",
+        "ちゅっちゅ！！",
+        "他の女と一緒にいないよね…？",
+        "あなたを誰にも渡さない……",
+        "ねー……",
+        "むぅ",
+        "いっぱいお話したいだけなのに……",
+        "ねぇってばー",
+        "すきだよ？",
+        "こんなに好きなのに……",
+        "私が悪いのかな……?",
+        "はぁ。。。",
+        "ふーん…",
+        "こんなにすきなのに……",
+        "あいたい。",
+        "ねぇ、会いたいよー……",
+    ]
     callback(null,{
         statusCode: 200,
         body: JSON.stringify({
-            foo: msg,
-            bar: 'Goodbye, world',
+            id: 0,
+            message: msgList[Math.floor( Math.random()* msgList.length )],
         })
     });
 }
