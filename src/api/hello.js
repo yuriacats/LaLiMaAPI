3672f0dceb0cc2874dd167116830429657301139
yuria <yuria.soft0615@gmail.com>
2020-10-25 22:01:48 +0900

    hello.jsの記法をAWSにあるGideに寄せてみた。ちなみにテストコードは無茶苦茶なはずなのになぜか通る。なんで

--- src/api/hello.js
+++ src/api/hello.js
@@ -1,6 +1,6 @@
-exports.handler = function(event ,context ,callback){
-  callback(null,{
+exports.handler = async function(){
+   return {
     statusCode: 200,
     body:'Hello World'
-  });
+  }
 }
